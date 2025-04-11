fb43fc1f442baf701ea4e0af08645a015a63740e
garzaro <clebergarzaro74@gmail.com>
2025-04-11 16:39:50 -0300

    cors

--- src/components/erro/autoDismissAlert.js
+++ src/components/erro/autoDismissAlert.js
@@ -1,15 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 
 const AutoDismissAlert = ({ message, type = "danger", duration = 3000 }) => {
     const [visible, setVisible] = useState(true);
+    const [fadeOut, setfadeOut] = useState(false);
 
     useEffect(() => {
-        const timer = setTimeout(() => setVisible(false), duration);
-        return () => clearTimeout(timer);
+        const fadeTimer = setTimeout(() => setVisible(true), duration);
+        const removeTimer = setTimeout(() => setfadeOut(false), duration + 500);
+
+        return () => {
+            clearTimeout(fadeTimer);
+            clearTimeout(removeTimer);
+        };
     }, [duration]);
     if (!visible) return null;
     return (
-        <div className={`alert alert-${type} fade show`} role="alert">
+        <div
+            className={`alert alert-${type} ${fadeOut ? "fade-out" : "fade show"}`}
+            role="alert"
+            style={{
+                transition: "opacity 0.5s ease-in-out",
+                backgroundColor: type === "danger" ? "#f8d7da" : undefined, // vermelho mais claro
+                color: type === "danger" ? "#721c24" : undefined,
+            }}
+        >
             {message}
         </div>
     );
