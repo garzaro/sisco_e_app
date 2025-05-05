f728cd44e2fa37f5bed32c8f8433d2629f0a18da
garzaro <clebergarzaro74@gmail.com>
2025-05-05 12:51:12 -0300

    usuarioService

---
+++ src/app/service/usuarioService.js
@@ -0,0 +1,15 @@
+import Apiservice from "../apiservice";
+
+const usuarioApi = Apiservice('/api/usuarios');
+
+const ServiceUsuario = () =>{
+    return{
+        autenticar: (credenciais) => {
+            return usuarioApi.post('/autenticar', credenciais);
+        },
+        salvarUsuario: (usuarios) => {
+            return usuarioApi.post(`/`, usuarios);
+        }
+    };
+};
+export default ServiceUsuario;
