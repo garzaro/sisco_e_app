cfc2941967d57e21f9e6560e21d05898d649c283
garzaro <clebergarzaro74@gmail.com>
2025-05-14 00:59:43 -0300

    refatorando a tela de cadastro

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import Card from "../components/card/card";
 import FormGroup from "../components/form/form-group";
-import {mensagemDeErro} from '../components/toastr'
+import {mensagemDeErro} from '../components/utils/toastr'
 import UsuarioService from "../app/service/usuarioService";
+import FormLayout from "../components/form/form-layout";
+import Layout from "../components/layout/layout";
 
 function LoginForm () {
     const [email, setEmail] = useState('');
@@ -30,48 +32,39 @@ function LoginForm () {
         navigate('/definirsenha');
     }
     return (
-        <div className="container-fluid mt-5 style={{minHeight: '0vh', display: 'flex', flexDirection: 'column', alignItens:'center'}}>}}" >
-            <div className="row justify-content-center w-100" >
-                <div className="col-md-6">
-                    <div className="bs-docs-section">
-                        <Card title="Login">
-                            <div className="row">
-                                <div className="col-lg-12">
-                                    <div className="bs-component">
+        <>
+            <Layout>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                            <div className="card border-0 shadow rounded-3 my-5">
+                                <div className="card-body p-4 p-sm-5">
+                                    <h5 className="card-title text-center mb-5 fw-light fs-5">Login</h5>
                                         <form onSubmit={handleSubmit(fazerLogin)}>
-                                            <FormGroup label={
-                                                <span>
-                                                Email:<span className="asterisco-vermelho">*</span>
-                                            </span>
-                                            } name={"email"}
-                                            >
-                                                {/* Campo E-mail */}
+                                            {/* Campo E-mail */}
+                                            <FormLayout>
                                                 <input
+                                                    {...register("email", {required: "O e-mail é obrigatório"})}
                                                     type="email"
-                                                    {...register("email", {required: "E-mail é obrigatório"})}
-                                                    className="form-control form-control-sm inputPlaceholder"
-                                                    placeholder="Digite seu email"
-                                                    id="email"
-                                                />
-                                                {errors.email && <span className="error">{errors.email.message}</span>}
-                                            </FormGroup>
-
-                                            <FormGroup label={
-                                                <span>
-                                                Senha:<span className="asterisco-vermelho">*</span>
-                                            </span>
-                                            } name={"senha"}
-                                            >
-                                                {/* Campo Senha */}
+                                                    className="form-control"
+                                                    id="floatingInput"
+                                                    placeholder="Digite o email"
+                                                    />
+                                                    <label className="floatingInput">Digite o e-mail<span className="asterisco-vermelho">*</span></label>
+                                                    {errors.email && <span className="error">{errors.email.message}</span>}
+                                            </FormLayout>
+                                            {/*campo de senha*/}
+                                            <FormLayout>
                                                 <input
+                                                    {...register("senha", {required: "A senha é obrigatória"})}
                                                     type="password"
-                                                    {...register("senha", {required: "Senha é obrigatória"})}
-                                                    className="form-control form-control-sm inputPlaceholder"
-                                                    placeholder="Digite sua senha"
+                                                    className="form-control"
+                                                    id="floatingInput"
+                                                    placeholder="Digite a senha"
                                                 />
-                                                {errors.senha && <span className="error-backend">{errors.senha.message}</span>}
-                                            </FormGroup>
-
+                                                <label className="floatingInput">Digite a senha<span className="asterisco-vermelho">*</span></label>
+                                                {errors.senha && <span className="error">{errors.senha.message}</span>}
+                                            </FormLayout>
                                             {/*esqueceu a senha*/}
                                             <div className="nav-signin-tooltip-footer link-info">Esqueceu a senha?
                                                 <a href="/register"
@@ -79,44 +72,44 @@ function LoginForm () {
                                                    aria-label="Esqueceu a senha? Clique aqui para criar uma nova.">&nbsp;
                                                     Clique aqui.</a>
                                             </div>
-
                                             {/* Botão de Login */}
-                                            <button
-                                                type="submit" className="btn btn-success btn-sm mt-3"
-                                                onClick={handleCancelar}>Entrar
-                                            </button>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </Card>
-                        {/*cadastro*/}
-                        <Card title="Seja bem vindo!">
-                            <div className="row">
-                                <div className="col-lg-12">
-                                    <div className="bs-component">
-                                        <div className="card-body">
-                                            <div className="m-sm-4">
-                                                <h2 className="text-center">Primeiro acesso?</h2>
+                                            <div className="d-grid">
+                                                <button type="submit"
+                                                        className="btn btn-primary btn-sm mt-3 text-uppercase fw-bold">
+                                                    Entrar
+                                                </button>
+                                            </div>
+                                            <hr className="my-4"></hr>
+                                            <div className="">
+                                                <h5 className="text-center">Primeiro acesso?</h5>
                                                 <p className="text-center mb-3">
                                                     Se ainda não possui acesso, clique no
-                                                    botão abaixo, crie sua conta e obtenha acesso ao Sistema de Consulta Escolar.</p>
+                                                    botão abaixo, crie sua conta e obtenha acesso ao Sistema de
+                                                    Consulta Escolar.
+                                                </p>
                                                 <div className="text-center">
                                                     <a href="/register" className="btn btn-sm btn-warning"
                                                        title="Não tem uma conta? Clique aqui!">Criar conta</a>
                                                 </div>
                                             </div>
-                                        </div>
+                                        </form>
                                     </div>
                                 </div>
                             </div>
-                        </Card>
-
+                        </div>
                     </div>
-                </div>
-            </div>
-        </div>
+            </Layout>
+            {/*cadastro*/}
+        </>
     );
 };
-
 export default LoginForm;
+
+
+
+
+
+
+
+
+
--- src/pages/register.js
+++ src/pages/register.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import {useForm} from "react-hook-form";
 import FormGroup from "../components/form/form-group";
 import UsuarioService from "../app/service/usuarioService";
-import {mensagemDeErro, mensagemDeSucesso} from '../components/toastr';
+import {mensagemDeErro, mensagemDeSucesso} from '../components/utils/toastr';
 import Astered from "../css/astered";
 import {Link, useNavigate} from "react-router-dom";
 import Layout from "../components/layout/layout";
@@ -44,7 +44,7 @@ function Register () {
         <Layout>
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                    <div className="col-sm-8 col-md-7 col-lg-6 mx-auto ">
                         <div className="card border-0 shadow rounded-3 my-5">
                             <div className="card-body p-4 p-sm-5">
                                 <h5 className="card-title text-center mb-5 fw-light fs-5">Criar nova conta</h5>
@@ -55,10 +55,10 @@ function Register () {
                                             {...register("nomeCompleto", {required: "O nome completo é obrigatório"})}
                                             type="text"
                                             className="form-control "
-                                            id="floatingInput"
+                                            id="floatingInputNome"
                                             placeholder="Nome Completo"
                                         />
-                                        <label htmlFor="floatingInput">Nome completo<span className="asterisco-vermelho">*</span></label>
+                                        <label className="">Nome completo<span className="asterisco-vermelho">*</span></label>
                                     </div>
                                     {/*campo cpf*/}
                                     <div className="form-floating mb-3">
@@ -66,10 +66,10 @@ function Register () {
                                             {...register("cpf", {required: "O cpf é obrigatório"})}
                                             type="number"
                                             className="form-control"
-                                            id="floatingInput"
+                                            id="floatingInputCpf"
                                             placeholder="CPF"
                                         />
-                                        <label htmlFor="floatingInput">Cadastro Pessoa Física<span className="asterisco-vermelho">*</span></label>
+                                        <label className="floatingInput">Cadastro Pessoa Física<span className="asterisco-vermelho">*</span></label>
                                     </div>
                                     {/*campo nome de usuario*/}
                                     <div className="form-floating mb-3">
@@ -77,33 +77,70 @@ function Register () {
                                             {...register("nomeUsuario", {required: "O nome de usuario é obrigatório"})}
                                             type="r"
                                             className="form-control"
-                                            id="floatingInput"
+                                            id="floatingInputUsuario"
                                             placeholder="Nome de usuario"
                                         />
-                                        <label htmlFor="floatingInput">Nome de usuario<span className="asterisco-vermelho">*</span></label>
+                                        <label className="floatingInput">Nome de usuario<span className="asterisco-vermelho">*</span></label>
                                     </div>
+                                    <hr className="my-4"></hr>
                                     {/*campo email*/}
-                                    <div className="form-floating mb-3">
-                                        <input
-                                            {...register("email", {required: "O email é obrigatório"})}
-                                            type="r"
-                                            className="form-control"
-                                            id="floatingInput"
-                                            placeholder="Email"
-                                        />
-                                        <label htmlFor="floatingInput">Email<span className="asterisco-vermelho">*</span></label>
+                                    <div className="row">
+                                        <div className="col-md-6 mb-3">
+                                            <div className="form-floating">
+                                                <input
+                                                    {...register("email", {required: "O email é obrigatório"})}
+                                                    type="email"
+                                                    className="form-control"
+                                                    id="floatingInputEmail"
+                                                    placeholder="Digite o email"
+                                                />
+                                                <label className="floatingInput">Digite o email<span className="asterisco-vermelho">*</span></label>
+                                            </div>
+                                        </div>
+                                        {/*campo confirmar email*/}
+                                        <div className="col-md-6 mb-3">
+                                            <div className="form-floating mb-3">
+                                                <input
+                                                    {...register("emailConfirmacao", {required: "Confirmação de email é obrigatório"})}
+                                                    type="email"
+                                                    className="form-control"
+                                                    id="floatingInputConfirmacaoEmail"
+                                                    placeholder="Confirmar email"
+                                                />
+                                                <label className="floatingInput">Confirmar email<span className="asterisco-vermelho">*</span></label>
+                                            </div>
+                                        </div>
                                     </div>
-                                    {/*campo confirmar email*/}
-                                    <div className="form-floating mb-3">
-                                        <input
-                                            {...register("emailConfirmacao", {required: "Confirmação de email é obrigatório"})}
-                                            type="email"
-                                            className="form-control"
-                                            id="floatingInput"
-                                            placeholder="Email"
-                                        />
-                                        <label htmlFor="floatingInput">Email<span className="asterisco-vermelho">*</span></label>
+                                    <hr className="my-4"></hr>
+                                    {/*campo senha*/}
+                                    <div className="row">
+                                        <div className="col-md-6 mb-3">
+                                            <div className="form-floating">
+                                                <input
+                                                    {...register("senha", {required: "A senha é obrigatória"})}
+                                                    type="password"
+                                                    className="form-control"
+                                                    id="floatingInputSenha"
+                                                    placeholder="Digite a senha"
+                                                />
+                                                <label className="floatingInput">Digite a senha<span className="asterisco-vermelho">*</span></label>
+                                            </div>
+                                        </div>
+                                        {/*campo confirmar senha*/}
+                                        <div className="col-md-6 mb-3">
+                                            <div className="form-floating mb-3">
+                                                <input
+                                                    {...register("senhaConfirmacao", {required: "Confirmação de senha é obrigatório"})}
+                                                    type="password"
+                                                    className="form-control"
+                                                    id="floatingInputConfirmacaoSenha"
+                                                    placeholder="Confirmar a senha"
+                                                />
+                                                <label className="floatingInput">Confirmar a senha<span className="asterisco-vermelho">*</span></label>
+                                            </div>
+                                        </div>
                                     </div>
+                                    <hr className="my-4"></hr>
                                     {/*botão*/}
                                     <div className="d-grid">
                                         <button
