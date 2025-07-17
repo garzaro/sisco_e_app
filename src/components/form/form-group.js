2cabcad76d9ba4bc63f927f7c058c760fd413669
garzaro <clebergarzaro74@gmail.com>
2025-07-17 16:36:46 -0300

    ajustes

--- src/components/form/form-group.js
+++ src/components/form/form-group.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-{/*addBreak = true* opcional = <br>*/}
+/*addBreak = true* opcional = <br>*/
 function FormGroup({htmlFor, label, children, addBreak = false}){
     return (
         <div className="form-group text-success" style={{ marginTop: '5px' }}>
