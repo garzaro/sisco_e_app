ccfb3b363d38c4c1edcf88d5d83220b6d7083da1
garzaro <clebergarzaro74@gmail.com>
2025-07-17 16:36:34 -0300

    ajustes

--- src/components/form/form-layout.js
+++ src/components/form/form-layout.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const FormLayout = ({htmlFor, label, children}) =>{
     return (
-        <div className="form-floating mb-3 text-success">
+        <div className="mb-3 text-success"> {/*form-floating*/}
             <label htmlFor={htmlFor} className="text-success">{label}</label>
             {children}
         </div>
